5dee2e648b692c9edea2650ae01aae6bd3d563c8
AlexandrIstomin <108895194+AlexandrIstomin@users.noreply.github.com>
2022-12-28 18:22:07 +0300

    рекурия задача1

---
+++ H.W_C#(1)/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число\n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Numbers (int number, int res){
+    if(res == 0){
+        return;
+    }
+    Numbers(number, res - 1);
+    Console.Write(number - res + 1 + " ");
+}
+Numbers(n, n);
